e8ffb62f6f59c96219d95a3795507c1c91a2e9f9
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-27 23:58:38 +0530

    Fetch particular user by id

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -4,16 +4,16 @@ import User from '../Model/userModel.js';
 export const create = async (req, res) => {
     try {
         const userData = new User(req.body);
-    
+
         if (!userData) {
-            return res.status(400).json({ message: "User data is required" }); 
+            return res.status(400).json({ message: "User data is required" });
         }
-        
+
         const saveData = await userData.save();
-        res.status(201).json(saveData); 
+        res.status(201).json(saveData);
     } catch (error) {
-        console.error("Error saving user data:", error); 
-        res.status(500).json({ error: error.message }); 
+        console.error("Error saving user data:", error);
+        res.status(500).json({ error: error.message });
     }
 };
 
@@ -23,15 +23,35 @@ export const create = async (req, res) => {
 export const getAll = async (req, res) => {
     try {
         const userData = await User.find();
-    
+
         if (!userData) {
-            return res.status(404).json({ message: "User data is not found" }); 
+            return res.status(404).json({ message: "User data is not found" });
         }
-        res.status(200).json(userData); 
-    } catch (error) { 
-        res.status(500).json({ error: error.message }); 
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
+
+//Api for 'fetch' particular user data
+
+export const getOne = async(req, res) => {
+    try {
+        // Extract the user ID from request parameters
+        const id = req.params.id;
+
+        // Use findById() to find a specific user by their ID
+        const userExist = await User.findById(id)
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(userExist);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
 
 
 
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create, getAll } from "../Controller/userController.js";
+import { create, getAll, getOne } from "../Controller/userController.js";
 
 const router = express.Router();
 
 router.post('/create', create);
 router.get('/getall', getAll);
+router.get('/getone/:id', getOne);
 
 export default router;
