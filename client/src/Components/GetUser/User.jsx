c9b90867dd38c70a5a5fcc7e844a815ab0162146
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-29 00:30:09 +0530

    Dummy table Created

--- client/src/Components/GetUser/User.jsx
+++ client/src/Components/GetUser/User.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './User.css';
+import { Link } from 'react-router-dom';
 
 const User = () => {
   return (
-    <div>User</div>
-  )
-}
+    <div className="user-table-container">
+      <div className="user-table-header">
+        <Link to="/add" className="add-user-btn">
+          Add User
+        </Link>
+      </div>
 
-export default User
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>S.No.</th>
+            <th>User Name</th>
+            <th>User Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Ankit Kumar</td>
+            <td>ankit@gmail.com</td>
+            <td>
+              <button className="action-btn delete-btn">Delete</button>
+              <Link to="/" className="action-btn edit-btn">
+                Edit
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default User;
