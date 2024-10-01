8b68e047d5fd803e798dc9d8e91bbe0954678b6f
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-01 21:40:46 +0530

    Hit Api for Add.jsx

--- client/src/Components/AddUser/Add.jsx
+++ client/src/Components/AddUser/Add.jsx
@@ -1,41 +1,101 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link} from 'react-router-dom';
+import axios from 'axios';
 import './Add.css';
+import toast from 'react-hot-toast';
 
 const Add = () => {
-  return (
-    <div className='addUser'>
-        <Link to={'/'}>Back</Link>
-        <h3>Add New User</h3>
-        <form>
-            <div className='inputGroup'>
-                <label htmlFor='fname'>First Name</label>
-                <input type='text' id='fname' name='fname' autoComplete='off' placeholder='Enter your first name '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <label htmlFor='lname'>Last Name</label>
-                <input type='text' id='lname' name='lname' autoComplete='off' placeholder='Enter your Last name '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' id='email' name='fname' autoComplete='off' placeholder='Enter your Email '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <label htmlFor='password'>Password</label>
-                <input type='password' id='password' name='fname' autoComplete='off' placeholder='Enter your Password '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <button type='submit'>Add User</button>
+
+    const initialUserState = {
+        fname: '',
+        lname: '',
+        email: '',
+        password: ''
+    };
+
+    const [user, setUser] = useState(initialUserState);
+    
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setUser(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        axios.post("http://localhost:8000/api/create", user)
+            .then((response) => {
+                const successMessage = response.data.msg || "User added successfully!";
+                toast.success(successMessage, { position: "top-center" });  // Fallback message
                 
-            </div>
-        </form>
+            })
+            .catch((error) => {
+                console.error(error);
+                toast.error("Failed to add user", { position: "top-center" });
+            });
+    };
 
-    </div>
-  )
-}
+    return (
+        <div className='addUser'>
+            <Link to={'/'}>Back</Link>
+            <h3>Add New User</h3>
+            <form onSubmit={submitForm}>
+                <div className='inputGroup'>
+                    <label htmlFor='fname'>First Name</label>
+                    <input
+                        type='text'
+                        id='fname'
+                        name='fname'
+                        value={user.fname}
+                        onChange={inputHandler}
+                        autoComplete='off'
+                        placeholder='Enter your first name'
+                    />
+                </div>
+                <div className='inputGroup'>
+                    <label htmlFor='lname'>Last Name</label>
+                    <input
+                        type='text'
+                        id='lname'
+                        name='lname'
+                        value={user.lname}
+                        onChange={inputHandler}
+                        autoComplete='off'
+                        placeholder='Enter your last name'
+                    />
+                </div>
+                <div className='inputGroup'>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        value={user.email}
+                        onChange={inputHandler}
+                        autoComplete='off'
+                        placeholder='Enter your email'
+                    />
+                </div>
+                <div className='inputGroup'>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        type='password'
+                        id='password'
+                        name='password'
+                        value={user.password}
+                        onChange={inputHandler}
+                        autoComplete='off'
+                        placeholder='Enter your password'
+                    />
+                </div>
+                <div className='inputGroup'>
+                    <button type='submit'>Add User</button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-export default Add
+export default Add;
+
+
