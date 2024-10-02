0fe7e40d09f699f73f4734eccaeaaba14aa6ac91
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-02 19:14:54 +0530

    Edit Added

--- client/src/Components/UpdateUser/Edit.jsx
+++ client/src/Components/UpdateUser/Edit.jsx
@@ -1,36 +1,96 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import './Edit.css';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+import toast from 'react-hot-toast';
 
 const Edit = () => {
+  const users = {
+    fname: "",
+    lname: "",
+    email: ""
+  };
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [user, setUser] = useState(users);
+
+  const inputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/getone/${id}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:8000/api/update/${id}`, user)
+    .then((response) => {
+        const successMessage = response.data.msg || "User Updated successfully!";
+        toast.success(successMessage, { position: "top-center" });  
+        navigate('/');
+    })
+    .catch((error) => {
+        console.error(error);
+        toast.error("Failed to Update user", { position: "top-center" });
+    });
+  };
+
   return (
     <div className='editUser'>
-        <Link to={'/'}>Back</Link>
-        <h3>Update User</h3>
-        <form>
-            <div className='inputGroup'>
-                <label htmlFor='fname'>First Name</label>
-                <input type='text' id='fname' name='fname' autoComplete='off' placeholder='Enter your first name '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <label htmlFor='lname'>Last Name</label>
-                <input type='text' id='lname' name='lname' autoComplete='off' placeholder='Enter your Last name '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' id='email' name='fname' autoComplete='off' placeholder='Enter your Email '>
-                </input>
-            </div>
-            <div className='inputGroup'>
-                <button type='submit'>Update User</button>
-                
-            </div>
-        </form>
-
+      <Link to="/">Back</Link>
+      <h3>Update user</h3>
+      <form className='editUserForm' onSubmit={submitForm}>
+        <div className="inputGroup">
+          <label htmlFor="fname">First name</label>
+          <input
+            type="text"
+            value={user.fname}
+            onChange={inputChangeHandler}
+            id="fname"
+            name="fname"
+            autoComplete='off'
+            placeholder='First name'
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="lname">Last name</label>
+          <input
+            type="text"
+            value={user.lname}
+            onChange={inputChangeHandler}
+            id="lname"
+            name="lname"
+            autoComplete='off'
+            placeholder='Last name'
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={user.email}
+            onChange={inputChangeHandler}
+            id="email"
+            name="email"
+            autoComplete='off'
+            placeholder='Email'
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="submit">UPDATE USER</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Edit
+export default Edit;
